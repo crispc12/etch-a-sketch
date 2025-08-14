8d94395636548200d0316914c9b4c4d94975d157
crispc12 <71873417+crispc12@users.noreply.github.com>
2025-08-14 20:22:41 +0200

    First aproximation

--- java.js
+++ java.js
@@ -1,7 +1,22 @@
 const container = document.querySelector("#container")
+// const tama = 100;
+const btn = document.querySelector("#btn1");
 
-for (let i = 0; i < 16; i++) {
-    let square = document.createElement("div")
-    square.setAttribute("class", "square")
-    container.appendChild(square)
+function drawGrid(tama){
+    for (let i = 0; i < tama*tama; i++) {
+        let square = document.createElement("div")
+        square.setAttribute("class", "square")
+        square.style.width = "calc(100vmin / " + tama + ")"
+        square.style.height = "calc(100vmin / " + tama + ")"
+        square.addEventListener("mouseenter", () => {
+            square.style.backgroundColor = "blueviolet";
+        })
+        container.appendChild(square)
+    }
 }
+btn.addEventListener("click", () => {
+    container.removeChild(square);
+    let tama = prompt("¿De qué dimensión quieres el grid?")
+    // container.removeChild(square)
+    drawGrid(tama);
+})
